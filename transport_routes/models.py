cf386dcc726f8b762bd33c7c3be9bf63880f56ed
Mark Lysenko <oomarkizoo@mail.ru>
2020-12-07 15:22:14 +0300

    Adds models of routes & transports
    Also adds migration files for them

--- transport_routes/models.py
+++ transport_routes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from stilsoft_test import constants
+
+ROUTE_TYPE = "Select the type of transport route"
+TRANSPORT_TYPE = "Select the type of transport"
+TRANSPORT_NUMBER = "Enter the board number of the vehicle"
+
+
+class Route(models.Model):
+    number = models.PositiveSmallIntegerField(default=None,
+                                              help_text="Enter route number")
+    transports = models.ManyToManyField('Transport')
+    route_type = models.CharField(max_length=5, choices=constants.TRANSPORT_TYPE,
+                                  help_text=ROUTE_TYPE, default='bus')
+
+    def __str__(self):
+        return '{0} / {1}'.format(self.route_type, self.number)
+
+    class Meta:
+        verbose_name = 'Route'
+        verbose_name_plural = 'Routes'
+
+
+class Transport(models.Model):
+    board_number = models.PositiveIntegerField(default=None,
+                                               help_text=TRANSPORT_NUMBER,
+                                               null=True, blank=True)
+    transport_type = models.CharField(max_length=5, choices=constants.TRANSPORT_TYPE,
+                                      help_text=TRANSPORT_TYPE, default='bus')
+
+    def __str__(self):
+        return '{0} / {1}'.format(self.transport_type, self.board_number)
+
+    class Meta:
+        verbose_name = 'Transport'
+        verbose_name_plural = 'Transports'
