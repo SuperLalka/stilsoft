5e955591e9586407fb72d1846164cfef8e272ee5
Mark Lysenko <oomarkizoo@mail.ru>
2020-12-07 16:53:18 +0300

    Configures project urlpatterns

---
+++ transport_routes/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from django.urls import include
+
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
+routerAPI = DefaultRouter()
+routerAPI.register(r'routes', views.RouteViewSet, basename='routes')
+routerAPI.register(r'transports', views.TransportViewSet, basename='transports')
+
+
+app_name = 'transport_routes'
+urlpatterns = [
+    url(r'^api/', include(routerAPI.urls)),
+]
