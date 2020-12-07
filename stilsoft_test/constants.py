a26ff63b311c181b8c5889ed22afc5b48794f8ba
Mark Lysenko <oomarkizoo@mail.ru>
2020-12-07 15:23:55 +0300

    Configures the admin panel & adds a file with constants

---
+++ stilsoft_test/constants.py
@@ -0,0 +1,10 @@
+
+TRANSPORT_TYPE = [
+    ('air', 'Aircraft'),
+    ('bus', 'Bus'),
+    ('tbus', 'Trolleybus'),
+    ('train', 'Train'),
+    ('tram', 'Tram'),
+]
+
+TRANSPORT_TYPE_LIST = ['air', 'bus', 'tbus', 'train', 'tram']
--- transport_routes/admin.py
+++ transport_routes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from transport_routes.models import (
+    Route, Transport
+)
+
+
+@admin.register(Route)
+class SubPagesArticleAdmin(admin.ModelAdmin):
+    list_display = ('number', 'route_type')
+    list_filter = ('route_type',)
+    list_per_page = 30
+
+
+@admin.register(Transport)
+class SubPagesSectionAdmin(admin.ModelAdmin):
+    list_display = ('board_number', 'transport_type')
+    list_filter = ('transport_type',)
+    search_fields = ['board_number']
+    list_per_page = 50
