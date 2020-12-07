89162081f63163f9176ec30a8f904167ba220b8c
Mark Lysenko <oomarkizoo@mail.ru>
2020-12-08 00:24:24 +0300

    Makes changes to the project model, add intermediate table for users reviews

--- transport_routes/models.py
+++ transport_routes/models.py
@@ -14,6 +14,7 @@ class Route(models.Model):
     transports = models.ManyToManyField('Transport')
     route_type = models.CharField(max_length=5, choices=constants.TRANSPORT_TYPE,
                                   help_text=ROUTE_TYPE, default='bus')
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return '{0} / {1}'.format(self.route_type, self.number)
@@ -24,14 +25,16 @@ class Route(models.Model):
 
 
 class Transport(models.Model):
-    board_number = models.PositiveIntegerField(default=None,
-                                               help_text=TRANSPORT_NUMBER,
-                                               null=True, blank=True)
+    vehicle_id_number = models.PositiveIntegerField(default=None,
+                                                    help_text=TRANSPORT_NUMBER,
+                                                    null=True, blank=True)
+    description = models.CharField(max_length=100, help_text="Enter a description of the transport",
+                                   null=True, blank=True)
     transport_type = models.CharField(max_length=5, choices=constants.TRANSPORT_TYPE,
                                       help_text=TRANSPORT_TYPE, default='bus')
 
     def __str__(self):
-        return '{0} / {1}'.format(self.transport_type, self.board_number)
+        return '{0} / {1}'.format(self.transport_type, self.vehicle_id_number)
 
     class Meta:
         verbose_name = 'Transport'
@@ -41,6 +44,19 @@ class Transport(models.Model):
 class User(AbstractUser):
     user_group = models.CharField(max_length=10, choices=constants.USER_GROUP,
                                   help_text='Select user group', default='Passenger')
+    reviews = models.ManyToManyField('Transport', through='PassengersReviews')
 
     def check_group(self, group):
-        return group is self.user_group
+        return group == self.user_group
+
+
+class PassengersReviews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transport = models.ForeignKey(Transport, on_delete=models.CASCADE)
+    rating = models.CharField(max_length=10, choices=constants.SELECT_RATING,
+                              help_text='Select user group', null=True, blank=True)
+    review_text = models.CharField(max_length=100, help_text="Enter your review text",
+                                   null=True, blank=True)
+
+    class Meta:
+        db_table = "transport_routes_passengers_reviews"
