98286f460c18d0afd50c9eba2234929a7167b32b
Mark Lysenko <oomarkizoo@mail.ru>
2020-12-07 15:24:46 +0300

    Adds a file with django-admin command for DB autocomplete

---
+++ transport_routes/management/commands/fill_db.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import random
+from datetime import datetime
+from django.core.management.base import BaseCommand
+
+from transport_routes.models import (
+    Route, Transport
+)
+from stilsoft_test import constants
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        route_objects = Route.objects.count()
+        if route_objects < 200:
+            while route_objects < 200:
+                route, status = Route.objects.get_or_create(number=random.randint(1, 9999),
+                                                            route_type=random.choice(constants.TRANSPORT_TYPE_LIST))
+                route_objects += 1
+
+                if status:
+                    for num in range(5):
+                        transport = Transport.objects.create(board_number=random.randint(111111, 999999),
+                                                             transport_type=route.route_type)
+                        transport.save()
+                        route.transports.add(transport)
+
+                        print(datetime.now(), 'transport {0} / {1} for route created'.format(transport.board_number,
+                                                                                             transport.transport_type))
+
+                print(datetime.now(), 'route {0} / {1} created'.format(route.number, route.route_type))
