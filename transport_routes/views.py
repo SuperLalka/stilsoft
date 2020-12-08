882e8e187dc127aac6840d528f5559a03a4fbd05
Mark Lysenko <oomarkizoo@mail.ru>
2020-12-08 15:37:50 +0300

    Adds FBV for demo page & method for adding passenger feedback

--- transport_routes/views.py
+++ transport_routes/views.py
@@ -1,8 +1,20 @@
-from rest_framework import viewsets, mixins
+from django.core.paginator import Paginator
+from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework import permissions as drf_permissions
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
 
 from . import models, permissions, serializers
+
+
+def index(request, page=None):
+    objects = models.Route.objects.all()
+    paginator = Paginator(objects, 100)
+    page = page if page else 1
+    return render(request, 'routes_page.html', context={
+        'paginator': paginator.page(page)
+    })
 
 
 class RouteViewSet(viewsets.ModelViewSet):
@@ -51,3 +63,20 @@ class TransportViewSet(viewsets.ModelViewSet):
             return [permission() for permission in self.permission_classes_by_action[self.action]]
         except KeyError:
             return [permission() for permission in self.permission_classes]
+
+    @action(methods=('post',), detail=True, permission_classes=[permissions.PassengerPermission])
+    def add_review(self, request, id):
+        rating = request.data.get('rating', False)
+        review_text = request.data.get('review_text', False)
+        if not rating and not review_text:
+            return HttpResponse('No data available for review')
+
+        review_data = {'rating': rating if rating else None,
+                       'review_text': review_text if review_text else None}
+
+        models.PassengersReviews.objects.create(
+            user=request.user,
+            transport_id=id,
+            **review_data
+        )
+        return HttpResponse('Review added')
