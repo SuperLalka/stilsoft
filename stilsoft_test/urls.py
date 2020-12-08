ec5120f27cb206fe995e1526eaa9c5b5b419e70a
Mark Lysenko <oomarkizoo@mail.ru>
2020-12-08 15:54:38 +0300

    Adds swagger urlpatterns

--- stilsoft_test/urls.py
+++ stilsoft_test/urls.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions as drf_permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Stilsoft solution API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(drf_permissions.AllowAny,),
+)
+
+swagger_urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
 
 tokens_url = [
     path('', TokenObtainPairView.as_view()),
@@ -17,5 +33,6 @@ tokens_url = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', include(tokens_url)),
+    *swagger_urlpatterns,
     path('', include('transport_routes.urls')),
 ]
